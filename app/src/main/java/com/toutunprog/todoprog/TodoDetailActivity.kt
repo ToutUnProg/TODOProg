c601ec174dd707938759080543cdb365bd43e3a5
ToutUnProg <tout.un.programme.yt@gmail.com>
2019-01-15 10:11:10 +0100

    Display todos details

--- app/src/main/java/com/toutunprog/todoprog/TodoDetailActivity.kt
+++ app/src/main/java/com/toutunprog/todoprog/TodoDetailActivity.kt
@@ -4,6 +4,11 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import com.toutunprog.todoprog.adapter.TodoItemAdapter
+import com.toutunprog.todoprog.repository.TodoListRepository
+import kotlinx.android.synthetic.main.activity_detail.*
 
 class TodoDetailActivity : AppCompatActivity() {
     companion object {
@@ -17,8 +22,12 @@ class TodoDetailActivity : AppCompatActivity() {
         }
     }
 
+    private lateinit var viewAdapter: RecyclerView.Adapter<*>
+    private lateinit var viewManager: RecyclerView.LayoutManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
 
         val todoListIndex = intent.getIntExtra(INTENT_TODOLIST_ID, -1)
         if (todoListIndex == -1) {
@@ -26,5 +35,17 @@ class TodoDetailActivity : AppCompatActivity() {
         }
 
         println("TodoDetailActivity created for list index : $todoListIndex")
+
+        viewManager = LinearLayoutManager(this)
+        val todoList = TodoListRepository().getTodoList(todoListIndex)
+        title = todoList.title
+        viewAdapter = TodoItemAdapter(todoList.items)
+
+        detail_todos_recyclerview.apply {
+            setHasFixedSize(true)
+            layoutManager = viewManager
+            adapter = viewAdapter
+        }
+
     }
 }
---
+++ app/src/main/java/com/toutunprog/todoprog/adapter/TodoItemAdapter.kt
@@ -0,0 +1,41 @@
+package com.toutunprog.todoprog.adapter
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.toutunprog.todoprog.R
+import com.toutunprog.todoprog.model.TodoItem
+
+class TodoItemAdapter(
+    private val myDataset: Array<TodoItem>
+) : RecyclerView.Adapter<TodoItemAdapter.ViewHolder>() {
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+        val textView: TextView = view as TextView
+
+        fun bind(todoItem: TodoItem) {
+            textView.text = todoItem.text
+
+        }
+    }
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.todos_textview, parent, false)
+
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount() = myDataset.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(myDataset[position])
+
+    }
+
+
+}
